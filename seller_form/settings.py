8fc9837d874c455cd47730e8fec1fbb9aa575abe
Oleksandr Vinnichuk <oleksandr.vinnichuk@harmonic-advance.com>
2023-04-30 18:56:18 +0300

    Add allowed host

--- seller_form/settings.py
+++ seller_form/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['seller-form.herokuapp.com']
 
 # Application definition
 
