1739e5ae84e669c33b379900a44a43672149f1b5
Oleksandr Vinnichuk <oleksandr.vinnichuk@harmonic-advance.com>
2023-04-30 21:54:44 +0300

    Add postgres url, add python runtime, whitenoise static

--- settings_heroku.py
+++ settings_heroku.py
@@ -1,4 +1,5 @@
 from seller_form.settings import *
+import dj_database_url
 
 DATABASES = {
     'default': {
@@ -6,3 +7,15 @@ DATABASES = {
         'NAME': 'seller-form',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+INSTALLED_APPS += 'whitenoise.runserver_nostatic'
+
+MIDDLEWARE += 'whitenoise.middleware.WhiteNoiseMiddleware'
